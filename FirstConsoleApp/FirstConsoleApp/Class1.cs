cb7738005b36d57aff810004e1e259864b43d72a
raihan-akther <raihanakther78@gmail.com>
2022-11-18 20:30:52 +0600

    Init

---
+++ FirstConsoleApp/FirstConsoleApp/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstConsoleApp
+{
+    class Class1
+    {
+    }
+}
---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+    class employee
+    {
+
+    }
+}
